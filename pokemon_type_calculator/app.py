9e5349238260f1301d0ecd6cfc91e22c8522bd1f
Horrell_Caleb <horrc927@hsd.k12.or.us>
2024-03-22 15:07:01 -0700

    added labels for all combo boxs and added the opions for the combo boxs

--- pokemon_type_calculator/app.py
+++ pokemon_type_calculator/app.py
@@ -23,14 +23,32 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+from PyQt6.QtGui import QPalette, QColor
 
-
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        super(MainWindow, self).__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("Pokemon type effectiveness calculator")
+
+        layout1 = QVBoxLayout()
+
+        layout1.addWidget(QLabel('what is the typing of the attack'))
+        attack_type_combo_box = QComboBox()
+        attack_type_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
+                                        "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
+        layout1.addWidget(attack_type_combo_box)
+        layout1.addWidget(QLabel('what is the first or only type of the defending pokemon'))
+        defense_type_one_combo_box = QComboBox()
+        defense_type_one_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
+                                        "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
+        layout1.addWidget(QComboBox())
+        layout1.addWidget(QLabel('what is the second type of the defending pokemon'))
+        defense_type_two_combo_box = QComboBox()
+        defense_type_two_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
+                                        "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
+        layout1.addWidget(QComboBox())
 
         layout = QVBoxLayout()
         widgets = [
@@ -39,17 +57,22 @@ class MainWindow(QMainWindow):
             QComboBox,
             QLabel,
         ]
-
         for w in widgets:
-            layout.addWidget(w())
-
+            layout.addWidget(w())   
         widget = QWidget()
-        widget.setLayout(layout)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
+        widget.setLayout(layout1)
         self.setCentralWidget(widget)
 
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
 
 app = QApplication(sys.argv)
 window = MainWindow()
