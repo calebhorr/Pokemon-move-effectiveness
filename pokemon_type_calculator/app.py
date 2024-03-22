7288703a3562acc3f0783047b7c084b447818191
Horrell_Caleb <horrc927@hsd.k12.or.us>
2024-03-22 15:16:38 -0700

    changed the layout of the widgets

--- pokemon_type_calculator/app.py
+++ pokemon_type_calculator/app.py
@@ -21,6 +21,7 @@ from PyQt6.QtWidgets import (
     QSpinBox,
     QTimeEdit,
     QVBoxLayout,
+    QHBoxLayout,
     QWidget,
 )
 from PyQt6.QtGui import QPalette, QColor
@@ -33,46 +34,37 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Pokemon type effectiveness calculator")
 
         layout1 = QVBoxLayout()
-
+        layout1.setSpacing(5)
+        layout2 = QVBoxLayout()
+        layout2.setSpacing(5)
+        layout3 = QVBoxLayout()
+        layout3.setSpacing(5)
+        layout4 = QHBoxLayout()
+        layout4.setSpacing(100)
         layout1.addWidget(QLabel('what is the typing of the attack'))
         attack_type_combo_box = QComboBox()
         attack_type_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
                                         "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
         layout1.addWidget(attack_type_combo_box)
-        layout1.addWidget(QLabel('what is the first or only type of the defending pokemon'))
+        layout2.addWidget(QLabel('what is the first or only type of the defending pokemon'))
         defense_type_one_combo_box = QComboBox()
         defense_type_one_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
                                         "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
-        layout1.addWidget(QComboBox())
-        layout1.addWidget(QLabel('what is the second type of the defending pokemon'))
+        layout2.addWidget(defense_type_one_combo_box)
+        layout3.addWidget(QLabel('what is the second type of the defending pokemon'))
         defense_type_two_combo_box = QComboBox()
         defense_type_two_combo_box.addItems(["Normal", "Fire", "Water", "Grass", "Flying", "Fighting", "Poison", "Electric", 
                                         "Ground", "Rock", "Psychic", "Ice", "Bug", "Ghost", "Steel", "Dragon", "Dark", "Fairy"])
-        layout1.addWidget(QComboBox())
+        layout3.addWidget(defense_type_two_combo_box)
 
-        layout = QVBoxLayout()
-        widgets = [
-            QComboBox,
-            QComboBox,
-            QComboBox,
-            QLabel,
-        ]
-        for w in widgets:
-            layout.addWidget(w())   
+        layout4.addLayout(layout1)
+        layout4.addLayout(layout2)
+        layout4.addLayout(layout3)
         widget = QWidget()
-        widget.setLayout(layout1)
+        widget.setLayout(layout4)
         self.setCentralWidget(widget)
 
 
-class Color(QWidget):
-
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(color))
-        self.setPalette(palette)
 
 app = QApplication(sys.argv)
 window = MainWindow()
