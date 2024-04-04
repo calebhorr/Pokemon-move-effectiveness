e176a6aa23ec49f71b3925c0df4fb179e5b382f5
Horrell_Caleb <horrc927@hsd.k12.or.us>
2024-04-04 15:26:46 -0700

    fixxed problem with teh calculator not displaying the right information

--- pokemon_type_calculator/app.py
+++ pokemon_type_calculator/app.py
@@ -24,6 +24,10 @@ from PyQt6.QtGui import QPalette, QColor
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        global attack_type_combo_box
+        global defense_type_one_combo_box
+        global defense_type_two_combo_box
+        global calculation_label
         super().__init__()
         super(MainWindow, self).__init__()
 
@@ -57,27 +61,18 @@ class MainWindow(QMainWindow):
         layout3.addWidget(defense_type_two_combo_box)
         layout3.addStretch()
 
+        
         layout4.addLayout(layout1)
+        layout4.addSpacing(50)
         layout4.addLayout(layout2)
+        layout4.addSpacing(50)
         layout4.addLayout(layout3)
+        
         calculate_button = QPushButton(text = "Calculate")
         calculate_button.setFixedSize(75,30)
         calculate_button.setCheckable(True)
         calculate_button.clicked.connect(self.button_was_pressed)
         layout5.addWidget(calculate_button)
-
-
-        self.attack_type = attack_type_combo_box.currentText()
-        self.defense_type_one = attack_type_combo_box.currentText()
-        self.defense_type_two = attack_type_combo_box.currentText()
-        
-        if self.defense_type_two == "none": 
-            self.defense_type_two = " "
-        else:
-            self.defense_type_two = "and " + self.defense_type_two
-        self.calculation_phrase = f"The {self.attack_type} move will deal 1x damage to the {self.defense_type_one} {self.defense_type_two} type pokemon"
-        
-        
 
         layout_app.addLayout(layout4)
         layout_app.addLayout(layout5)
@@ -86,6 +81,34 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
     def button_was_pressed(self):
+        self.attack_type = attack_type_combo_box.currentText()
+        self.defense_type_one = defense_type_one_combo_box.currentText()
+        self.defense_type_two = defense_type_two_combo_box.currentText()
+        
+        attack_type = self.attack_type
+        defense_type_one = self.defense_type_one
+        defense_type_two = self.defense_type_two
+        pokemon_types = [
+            
+
+
+
+
+
+
+
+
+            
+        ]
+
+
+        if defense_type_two == "None": 
+            defense_type_two = ""
+        elif defense_type_one != defense_type_two:
+            defense_type_two = "and " + defense_type_two + " "
+        elif defense_type_one == defense_type_two:
+            defense_type_two = ""
+        self.calculation_phrase = f"The {attack_type} move will deal 1x damage to the {defense_type_one} {defense_type_two}type pokemon"
         print(self.calculation_phrase)
 
 
